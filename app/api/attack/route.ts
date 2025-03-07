a01a3bf56579894f40998ecc107581dac2c6e66f
aqua <84078890+phthallo@users.noreply.github.com>
2025-03-07 16:56:55 -0500

    fixes

--- app/api/attack/route.ts
+++ app/api/attack/route.ts
@@ -2,6 +2,7 @@
 // Start or end an attack session
 
 import { NextRequest, NextResponse } from "next/server";
+import { auth } from "@/auth";
 
 export default function POST({request, response}: {request: NextRequest, response: NextResponse}){
 }
