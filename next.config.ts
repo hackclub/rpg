f9b949c2c713ca42188af7a7777ea66ce8200f71
aqua <84078890+phthallo@users.noreply.github.com>
2025-03-14 11:05:54 -0400

    try to fix deployment errors

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,12 @@
 import type { NextConfig } from "next";
-
+const path = require('path')
 const nextConfig: NextConfig = {
   /* config options here */
-  output: "standalone"
+  output: "standalone",
+  webpack(config, options) {
+    config.resolve.alias["@/components"] = path.resolve(__dirname, "components");
+    return config;
+  }
 };
 
 export default nextConfig;
