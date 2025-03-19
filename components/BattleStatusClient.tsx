5275484fb31302f71bb81e1e4ab28646961d9385
aqua <84078890+phthallo@users.noreply.github.com>
2025-03-19 14:41:25 -0400

    limit active users popup size

--- components/BattleStatusClient.tsx
+++ components/BattleStatusClient.tsx
@@ -7,7 +7,7 @@ export function ActiveUsers({currentAdventurers}: {currentAdventurers: User[]}){
     const count = currentAdventurers.length
     return (
     <>
-        <Tooltip id="online"/>
+        <Tooltip className = "max-w-1/2" id="online"/>
         <span><span data-tooltip-id="online" data-tooltip-content={ currentAdventurers.map((adventurer: User) => adventurer.name).join(", ") }>{count} {count == 1 ? " adventurer" : " adventurers" }</span> currently on {count == 1 ? " a quest" : " quests" }!</span>
     </>
     )
