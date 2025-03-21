f5184bf1faf0457a1dddbbabdd1537d9d866ebd5
aqua <84078890+phthallo@users.noreply.github.com>
2025-03-21 18:59:17 -0400

    sigh

--- components/common/Info.tsx
+++ components/common/Info.tsx
@@ -5,7 +5,6 @@ import useSound from "use-sound";
 import Link from "next/link";
 import { useSession } from "next-auth/react";
 import Log from "../Log";
-import { shineEffect } from "@/lib/general";
 
 const Music = ({audio, play, stop, setAudio}: {audio: boolean, play: () => void, stop: () => void, setAudio: (value: any) => void}) => {
     return (
@@ -47,6 +46,13 @@ export function Info(){
                 <button onClick={() => setLogIsOpen(true)}>
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth={1.5} strokeLinecap="round" strokeLinejoin="round" className="lucide lucide-scroll-text"><path d="M15 12h-5"/><path d="M15 8h-5"/><path d="M19 17V5a2 2 0 0 0-2-2H4"/><path d="M8 21h12a2 2 0 0 0 2-2v-1a1 1 0 0 0-1-1H11a1 1 0 0 0-1 1v1a2 2 0 1 1-4 0V5a2 2 0 1 0-4 0v2a1 1 0 0 0 1 1h3"/></svg>
                 </button> : null }
+
+                <button onClick={() => setHelpIsOpen(true)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 5.25h.008v.008H12v-.008Z" />
+                    </svg>
+                </button>
+
             <Music audio={audio} setAudio={setAudio} play={play} stop={stop}/>
         </div>
 
